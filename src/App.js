13bac315e0c70245453040fbd56a26e81f6a32d1
Greg <gkmarsella@gmail.com>
2019-12-03 20:53:05 -0500

    added artist search function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import ArtistSearch from './search/ArtistSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listennotes_key = process.env.REACT_APP_LISTENNOTES_KEY;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [],
+      searchValue: ''
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({searchValue: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    let query = this.state.searchValue;
+    axios.get('https://listen-api.listennotes.com/api/v2/search?', { 
+      headers: {
+        "X-ListenAPI-Key":listennotes_key
+      },
+      params: {
+        'q': query
+      }
+    })
+      .catch( error => {
+        console.log('no results', error)
+      })
+      .then(response => {
+        this.setState({
+          searchResults: response.data.results,
+          searchValue: ''
+        })
+      })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' value={this.state.searchValue} onChange={this.onChange}/>
+          <button>Search</button>
+        </form>
+        <ArtistSearch results={this.state.searchResults}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/search/ArtistSearch.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ArtistSearch extends Component {
+    render () {
+        const Results = (this.props.results)
+            .map( result => {
+                return (
+                    <div>
+                        <li>
+                            <img src={result.image} />
+                            <span>Author: {result.podcast_title_original}</span>
+                            <span>Episode: {result.title_original}</span>
+
+                        </li>
+                    </div>
+                );
+            });
+        
+            return (
+                <div>
+                    <ul>
+                        {Results}
+                    </ul>
+                </div>
+            )
+    }
+}
+
+export default ArtistSearch;
